0d3158bde144180c4a66e0512b9a1ccf49f20692
my-avenue <alissanga@gmail.com>
2016-11-18 16:27:46 +0700

    .

---
+++ find_the_most_senior_developer.js
@@ -0,0 +1,23 @@
+const findSenior = (list) =>{
+  const maxage = list.reduce((result, {age: age}) => age>=result ? age : result ,0)
+  return list.filter(({age: age}) => age==maxage )
+}
+
+
+
+const list = [
+  { firstName: 'Gabriel', lastName: 'X.', country: 'Monaco', continent: 'Europe', age: 49, language: 'PHP' },
+  { firstName: 'Odval', lastName: 'F.', country: 'Mongolia', continent: 'Asia', age: 38, language: 'Python' },
+  { firstName: 'Emilija', lastName: 'S.', country: 'Lithuania', continent: 'Europe', age: 19, language: 'Python' },
+  { firstName: 'Sou', lastName: 'B.', country: 'Japan', continent: 'Asia', age: 49, language: 'PHP' },
+]
+
+
+console.log(findSenior(list))
+
+/*answer
+[
+  { firstName: 'Gabriel', lastName: 'X.', country: 'Monaco', continent: 'Europe', age: 49, language: 'PHP' },
+  { firstName: 'Sou', lastName: 'B.', country: 'Japan', continent: 'Asia', age: 49, language: 'PHP' },
+]
+*/
